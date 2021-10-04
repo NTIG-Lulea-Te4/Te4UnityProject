bcbe7cfb4032b0fff09c41fbc94a8cdff5d5dae5
MnHed <mans.hedlund@elev.ga.ntig.se>
2021-10-04 15:19:04 +0200

    10-04 Måns
    terrain + script

--- Assets/Måns/MånsScripts/MånsScriptPlatform.cs
+++ Assets/Måns/MånsScripts/MånsScriptPlatform.cs
@@ -7,12 +7,44 @@ public class MånsScriptPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 myPosition = GameObject.Find("Platform").transform.position;
+        float mySpeed = 5;
+        float myStationary = 0;
         
     }
 
     // Update is called once per frame
-    void Update()
+    void Update(Vector3 myPos, float speed, float stay)
     {
         
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            myPos = myPos;        
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+
+
+
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+
+
+
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+
+
+
+        }
+    
+    
     }
 }
