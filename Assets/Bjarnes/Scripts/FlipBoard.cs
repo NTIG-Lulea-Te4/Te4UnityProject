9755cfca1c36f24cedc9c09d4e7804a118b00620
Bjarne Lundqwist <bjarne@lundqwist.se>
2021-10-04 13:07:05 +0200

    test

--- Assets/Bjarnes/Scripts/FlipBoard.cs
+++ Assets/Bjarnes/Scripts/FlipBoard.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class FlipBoard : MonoBehaviour
 {
+
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,11 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            GameObject thisBoard = ge
+;
+        }
     }
 }
