a73cafb627d5c338d08bcbbd7e03ec512e5c26b0
Trueillusion1999 <true.illusion1999@gmail.com>
2021-10-04 15:25:14 +0200

    a

--- Assets/Mohammad/Scripts/NewBehaviourScript.cs
+++ Assets/Mohammad/Scripts/NewBehaviourScript.cs
@@ -13,6 +13,11 @@ public class NewBehaviourScriptNew : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+
         
     }
 }
