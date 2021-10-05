2d2c214fd7b0cfbae4d78a206a5ddf45473ed4db
Queen <ruben.bergenstrale@elev.ga.ntig.se>
2021-10-05 15:23:47 +0200

    Kod ändringar och felaktiga försök.

--- Assets/Rubens/Scripts/RotatingPlatform.cs
+++ Assets/Rubens/Scripts/RotatingPlatform.cs
@@ -7,29 +7,45 @@ public class RotatingPlatform : MonoBehaviour
     public Vector3 currentRotation;
     //stores x,y and z values.
 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         currentRotation = GetComponent<Transform>().eulerAngles;
         //current rotation, what is the current rotation? eulerAngles.
-
-        if ((Input.GetAxis("Horizontal")>.1) && (currentRotation.z <=9 || currentRotation.z >=351)) // gå bara mellan 351° och 9° dvs 351 - 360, 0(360) - 9.
+        if (Input.GetKey("up"))
         {
-            transform.Rotate(0, 0, -.1f);
+
+
+            if ((Input.GetAxis("Horizontal") > .1) && (currentRotation.z <= 9 || currentRotation.z >= 351)) // gå bara mellan 351° och 9° dvs 351 - 360, 0(360) - 9.
+            {
+                transform.Rotate(0, 0, -.1f);
+            }
+
+        }
+        else
+        {
+            while(currentRotation !<= 0)
+            {
+                transform.Rotate(0, 0, .1f);
+            } // https://www.codegrepper.com/code-examples/csharp/How+to+hold+key+down+unity
+
         }
 
-        if ((Input.GetAxis("Horizontal") < -.1)  && (currentRotation.z >= 350 || currentRotation.z <= 8))
+        if ((Input.GetAxis("Horizontal") < -.1) && (currentRotation.z >= 350 || currentRotation.z <= 8))
         {
             transform.Rotate(0, 0, .1f);
         }
 
-        if ((Input.GetAxis("Vertical") > .1)  && (currentRotation.x <= 8 || currentRotation.x >= 350))
+        if ((Input.GetAxis("Vertical") > .1) && (currentRotation.x <= 8 || currentRotation.x >= 350))
         {
             transform.Rotate(.1f, 0, 0);
         }
@@ -40,9 +56,6 @@ public class RotatingPlatform : MonoBehaviour
         }
 
 
-        while (currentRotation.y != 0)
-        {
-            currentRotation.y = 0;
-        }
+
     }
 }
