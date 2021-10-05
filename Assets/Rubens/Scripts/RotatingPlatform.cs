3ed8b73caa7d1ff9adedbca5ad0a7408b757c2ba
Queen <ruben.bergenstrale@elev.ga.ntig.se>
2021-10-05 13:31:52 +0200

    skapat detaljerad nivå

---
+++ Assets/Rubens/Scripts/RotatingPlatform.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingPlatform : MonoBehaviour
+{
+    public Vector3 currentRotation;
+    //stores x,y and z values.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentRotation = GetComponent<Transform>().eulerAngles;
+        //current rotation, what is the current rotation? eulerAngles.
+
+        if ((Input.GetAxis("Horizontal")>.1) && (currentRotation.z <=9 || currentRotation.z >=351)) // gå bara mellan 351° och 9° dvs 351 - 360, 0(360) - 9.
+        {
+            transform.Rotate(0, 0, -.1f);
+        }
+
+        if ((Input.GetAxis("Horizontal") < -.1)  && (currentRotation.z >= 350 || currentRotation.z <= 8))
+        {
+            transform.Rotate(0, 0, .1f);
+        }
+
+        if ((Input.GetAxis("Vertical") > .1)  && (currentRotation.x <= 8 || currentRotation.x >= 350))
+        {
+            transform.Rotate(.1f, 0, 0);
+        }
+
+        if ((Input.GetAxis("Vertical") < -.1) && (currentRotation.x >= 351 || currentRotation.x <= 9))
+        {
+            transform.Rotate(-.1f, 0, 0);
+        }
+
+
+        while (currentRotation.y != 0)
+        {
+            currentRotation.y = 0;
+        }
+    }
+}
